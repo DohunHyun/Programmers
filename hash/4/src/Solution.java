472b44b7b5f138ce54c80e202d906a3d3423339b
DohunHyun <dohun.hyun@gmail.com>
2020-06-10 16:08:17 +0900

    studying about hash and dfs,bfs

--- hash/4/src/Solution.java
+++ hash/4/src/Solution.java
@@ -4,103 +4,75 @@ public class Solution {
 
     public static void main(String[] args) {
         System.out.println("hi");
-        String[] genres = {"classic", "pop", "classic", "classic", "pop", "hiphop"};
-        int[] plays = {500, 600, 150, 800, 2500, 5000};
+        String[] genres = {"classic", "pop", "classic", "classic", "pop", "hiphop", "hiphop"};
+        int[] plays = {500, 600, 150, 800, 2500, 5000, 5000};
 
         System.out.println(solution(genres, plays));
 
     }
 
     public static int[] solution(String[] genres, int[] plays) {
-        int[] answer;
-        int answersize = 0;
+        List<Integer> answer = new ArrayList();
+        int size = 0;
 
-        HashMap<String, Integer> map = new HashMap<String, Integer>(); // genre / plays
-        for (int i = 0; i < genres.length; i++) {
-            map.put(genres[i], (int) map.getOrDefault(genres[i], 0) + plays[i]);
+        //장르 순서 정하기
+        HashMap<String, Integer> hashMap = new HashMap();
+        for(int i=0; i<genres.length; i++) {
+            hashMap.put(genres[i], hashMap.getOrDefault(genres[i], 0) + plays[i]);
         }
 
-        HashMap<Integer, Integer> indexMap = new HashMap<>(); // index / plays
-        for (int i = 0; i < plays.length; i++) {
-            indexMap.put(i, plays[i]);
-        }
-        // 위에서 장르별로 재생수 총합 끝. 클래식 1450, 팝 3100
+        //장르 순서 정렬하기
+        List list = new ArrayList();
+        list.addAll(hashMap.keySet());
+        Collections.sort(list, new Comparator<Object>() {
+            public int compare(Object o1, Object o2) {
+                Object v1 = hashMap.get(o1);
+                Object v2 = hashMap.get(o2);
 
-        List<Map.Entry<String, Integer>> list = new LinkedList<>(map.entrySet()); // 장르별 재생순서 정렬
-
-        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
-            @Override
-            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
-                return -o1.getValue().compareTo(o2.getValue());
+                return ((Comparable) v2).compareTo(v1);
             }
         });
 
-        List<Map.Entry<Integer, Integer>> playslist = new LinkedList<>(indexMap.entrySet()); // 인덱스별 재생횟수 순위
+        //각 장르마다 2곡씩 넣어서 만들기
+        for(int i=0; i<list.size(); i++) {
+            HashMap<Integer,Integer> tempHashmap = new HashMap(); //pop에 대해서 맵 만들고
 
-        Collections.sort(playslist, new Comparator<Map.Entry<Integer, Integer>>() {
-            @Override
-            public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
-                return -o1.getValue().compareTo(o2.getValue());
-            }
-        });
-
-        // 정렬 끝.
-        // 첫번째 장르에 대한 값들을 새롭게 맵에 저장. 장르가 같으면 (횟수,인덱스)로 저장.
-        // 횟수 높은 순으로 정렬 후 인덱스 따로 저장.
-
-        answer = new int[list.size() * 2];
-        for (int i = 0; i < list.size(); i++) {
-
-        }
-
-        for (Map.Entry entry : list) { // 리스트에 순서대로 가장 많이 재생한 2곡씩 순서대로 저장.
-            //entry.getkey 와 같은 장르를 가진 노래들 중에 재생수 높은애들로 저장.
-
-
-            for (int i = 0; i < plays.length; i++) {
-                if (genres[i].equals(entry.getKey())) { // i = 고유번호
-
+            for(int j=0; j<genres.length; j++) {
+                if(genres[j].equals(list.get(i))) { //장르가 pop이면
+                    tempHashmap.put(j, plays[j]); //그 재생수랑 인덱스 저장
                 }
             }
+            //다 저장하고 정렬한다.
+            List templist = new ArrayList();
+            templist.addAll(tempHashmap.keySet());
+            Collections.sort(templist, new Comparator<Object>() {
+                public int compare(Object o1, Object o2) {
+                    Object v1 = tempHashmap.get(o1);
+                    Object v2 = tempHashmap.get(o2);
+                    if(v1.equals(v2)) {
+                        return ((Comparable<Object>) o1).compareTo(o2); //오름차순
+                    } else {
+                        return ((Comparable<Object>) v2).compareTo(v1); //내림차순
+                    }
+                }
+            });
+
+
+            for(int n=0; n<2; n++) {
+                if(tempHashmap.size() > n) {
+                    answer.add((int)templist.get(n));
+                }
+            }
+
+            System.out.println();
         }
 
-//        for (int j = 0; j < 2; j++) { // 두번 돌면서 장르 2개
-//            Map firstmap = new HashMap<Integer, Integer>(); // 한 장르에 대한 맵 생성
-//            for (int i = 0; i < genres.length; i++) {
-//                if (genres[i].equals(list.get(j).getKey())) { // 장르가 1등(2등)이면
-//                    firstmap.put(plays[i], i); //재생횟수와 인덱스 저장
-//                }
-//            }
-//
-//            TreeMap<Integer, Integer> treeMap = new TreeMap<>(firstmap);
-//            Iterator iterator = treeMap.descendingKeySet().iterator();
-//
-//            int k;
-//            int v;
-//
-//            int cycle = 2;
-//            while (iterator.hasNext()) {
-//                if (answersize > 3) break;
-//                if (cycle == 0) break;
-//                k = (int) iterator.next();
-//                v = (int) treeMap.get(k);
-//                answer[answersize++] = v;
-//                cycle--;
-//            }
-//
-//            for(Object value : treeMap.values()) {
-//                if(answersize > 3) break;
-//                if(cycle == 0) break;
-//                answer[answersize++] = (int)value;
-//                cycle--;
-//            }
-//        }
 
-
-
-
-
-        return answer;
+        int[] answerArray = new int[answer.size()];
+        for(int temp : answer) {
+            answerArray[size++] = temp;
+        }
+        return answerArray;
 }
 
 
