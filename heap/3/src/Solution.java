8307ae546515df460f4732e02cd95c3f94ab5d4c
DohunHyun <dohun.hyun@gmail.com>
2020-07-27 22:11:06 +0900

    start solving

---
+++ heap/3/src/Solution.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[][] jobs = {{0,3}, {1,9}, {2,6}};
+
+        System.out.println(solution(jobs));
+    }
+
+    public static int solution(int[][] jobs) {
+        int answer = 0;
+        int[] array = new int[jobs.length];
+
+        Arrays.sort(array);
+
+        for(int i=0; i<jobs.length; i++) {
+            
+        }
+
+
+
+
+        return answer;
+    }
+}
