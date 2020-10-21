54431b9176fdb261be3a05f1c7d3489773617296
DohunHyun <dohun.hyun@gmail.com>
2020-10-21 14:56:22 +0900

    solved problem with dfs recursion

--- dfs,bfs/4/src/Solution.java
+++ dfs,bfs/4/src/Solution.java
@@ -1,60 +1,58 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Solution {
     public static void main(String[] args) {
-
-        String[][] tickets = {{"ICN","SFO"}, {"ICN", "ATL"}, {"SFO", "ATL"}, {"ATL", "ICN"}, {"ATL", "SFO"}};
-
-//        System.out.println(solution(tickets));
-//
-//        if( 0 > "ICN".compareTo("SFO") ) {
-//            System.out.println( "yes");
-//        }
-
-
+        String[][] tickets = {{"ICN", "SFO"}, {"ICN", "ATL"}, {"SFO", "ATL"}, {"ATL", "ICN"}, {"ATL", "SFO"}};
+        System.out.println(solution(tickets));
     }
+
+        static List<String> list = new ArrayList<>();
+        static boolean visited[];
+        static String route = "";
 
     public static String[] solution(String[][] tickets) {
 
+        for(int i=0; i<tickets.length; i++) {
+            visited = new boolean[tickets.length];
+            String from = tickets[i][0];
+            String dest = tickets[i][1];
 
-
-        int size = tickets.length;
-        int inputsize = 0;
-        String[] answer = new String[size+1];
-        answer[0] = "ICN";
-        boolean[] visited = new boolean[size]; // 기본 false
-        int visit = 0;
-
-        String dest = "";
-        String from = "ICN";
-
-        while(!visitcheck(visited)) {
-            for(int j=0; j<size; j++) {
-                if(!visited[j] && tickets[j][0].equals(from)) { // 출발지가 같으면
-                    if(0 > tickets[j][1].compareTo(dest)) { // 이미 저장된 도착지랑 알파벳 비교
-                        dest = tickets[j][1];
-                        visit = j;
-                    } else if(dest.equals("")) {
-                        dest = tickets[j][1]; // 도착지 입력.
-                        visit = j;
-                    }
-
-                }
+            if(from.equals("ICN")) { // 인천이면 시작 가능
+                route = from + ",";
+                visited[i] = true; // 갔던곳은 true 로
+                dfs(tickets, dest, 1);
             }
-            visited[visit] = true; // 이미 쓴 티켓 표시
-            from = dest; // 출발지 바꿔주고
-            dest = "";
-            answer[++inputsize] = from;
         }
+
+        Collections.sort(list);
+        String[] answer = list.get(0).split(",");
         return answer;
     }
 
-    private static boolean visitcheck(boolean[] visited) {
-        for(boolean check : visited) {
-            if(!check) { // false 이면, 즉 아직 방문안한게 있다
-                return false;
+    private static void dfs(String[][] tickets, String dest, int cnt) {
+        route += dest + ",";
+
+        if(cnt == tickets.length) { // 모든 국가를 다 갔을때,
+            list.add(route);
+            return;
+        }
+
+        for(int i=0; i<tickets.length; i++) { // 다음 경로 찾기
+            String f = tickets[i][0];
+            String d = tickets[i][1];
+
+            if(dest.equals(f) && !visited[i]) {
+                visited[i] = true;
+                dfs(tickets, d, cnt+1);
+                visited[i] = false;
+                route = route.substring(0, route.length()-4);
             }
         }
-        return true;
     }
 }
+
+
 
 
